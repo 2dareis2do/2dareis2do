5893e7bd3d9ade8b771969ab18e6f958fb04447a
Daniel Lobo <info@danlobo.co.uk>
2022-02-22 05:42:26 +0000

    - restrict login link to logged in admin users - put time limit on this as isAuthenticated does not seem reliable - add permisisons

--- src/ComputedField.php
+++ src/ComputedField.php
@@ -5,6 +5,7 @@ namespace Drupal\virtual_edit_link_field;
 use Drupal\Core\Field\FieldItemList;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\TypedData\ComputedItemListTrait;
+use Drupal\user\Entity\User;
 
 class ComputedField extends FieldItemList implements FieldItemListInterface {
 
@@ -14,11 +15,21 @@ class ComputedField extends FieldItemList implements FieldItemListInterface {
    * Compute the values.
    */
   protected function computeValue() {
-    $edit_node_id = $this->parent->getEntity()->id();
-    $edit_entity_type_bundle = $this->parent->getEntity()->getEntityTypeId();
-    $edit_base_url = \Drupal::request()->getSchemeAndHttpHost();
-    $editurl =  $edit_base_url . "/" . $edit_entity_type_bundle . "/" . $edit_node_id ."/edit/";
-    $this->list[0] = $this->createItem(0, $editurl);
+    
+    // lets check if admin user has logged in in the last hour
+    // isAuthenticated() seems to be abailable even if we are not logged in?
+    $admin = User::load(1);
+    $nowtime = time();
+    $last_login_date = (int) $admin->getLastAccessedTime();
+    $delta = $nowtime - $last_login_date;
+
+    if ($admin->isAuthenticated() && $admin->hasPermission('First level permission') && $delta <= (60 * 60)) {
+      $edit_node_id = $this->parent->getEntity()->id();
+      $edit_entity_type_bundle = $this->parent->getEntity()->getEntityTypeId();
+      $edit_base_url = \Drupal::request()->getSchemeAndHttpHost();
+      $editurl =  $edit_base_url . "/" . $edit_entity_type_bundle . "/" . $edit_node_id ."/edit/";
+      $this->list[0] = $this->createItem(0, $editurl);
+    }
   }
   
 }
