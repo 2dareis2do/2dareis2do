ad34e7299733432007dbb64e45a35feb5f169662
Daniel Lobo <info@danlobo.co.uk>
2022-02-22 22:33:19 +0000

    - remove use of isAutheticated (does not do what I want) - rename permissions

--- src/ComputedField.php
+++ src/ComputedField.php
@@ -17,13 +17,12 @@ class ComputedField extends FieldItemList implements FieldItemListInterface {
   protected function computeValue() {
     
     // lets check if admin user has logged in in the last hour
-    // isAuthenticated() seems to be abailable even if we are not logged in?
     $admin = User::load(1);
-    $nowtime = time();
-    $last_login_date = (int) $admin->getLastAccessedTime();
-    $last_logged_in = $nowtime - $last_login_date;
+    $now_timestamp = time();
+    $last_login_date_timestamp = (int) $admin->getLastAccessedTime();
+    $secs_since_last_logged_in = $now_timestamp - $last_login_date_timestamp;
 
-    if ($admin->isAuthenticated() && $admin->hasPermission('First level permission') && $last_logged_in <= (60 * 60)) {
+    if ($admin->hasPermission('Access Edit Link Field') && $secs_since_last_logged_in <= (60 * 60 * 24)) {
       $edit_node_id = $this->parent->getEntity()->id();
       $edit_entity_type_bundle = $this->parent->getEntity()->getEntityTypeId();
       $edit_base_url = \Drupal::request()->getSchemeAndHttpHost();
