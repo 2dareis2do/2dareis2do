eaada6230010b6cdeb60bb2341e53b05ca59d805
Daniel Lobo <info@danlobo.co.uk>
2022-03-02 12:23:50 +0000

    - update to use use entitiees toUrl method

--- src/ComputedField.php
+++ src/ComputedField.php
@@ -16,17 +16,18 @@ class ComputedField extends FieldItemList implements FieldItemListInterface {
    */
   protected function computeValue() {
     
-    // lets check if admin user has logged in in the last hour
+    // lets check if admin user has logged in in the last day
     $admin = User::load(1);
     $now_timestamp = time();
     $last_login_date_timestamp = (int) $admin->getLastAccessedTime();
     $secs_since_last_logged_in = $now_timestamp - $last_login_date_timestamp;
-
     if ($admin->hasPermission('Access Edit Link Field') && $secs_since_last_logged_in <= (60 * 60 * 24)) {
-      $edit_node_id = $this->parent->getEntity()->id();
-      $edit_entity_type_bundle = $this->parent->getEntity()->getEntityTypeId();
-      $edit_base_url = \Drupal::request()->getSchemeAndHttpHost();
-      $editurl =  $edit_base_url . "/" . $edit_entity_type_bundle . "/" . $edit_node_id ."/edit/";
+      $options = [
+        'absolute' => TRUE,
+        'language' => \Drupal::languageManager()->getCurrentLanguage(),
+      ];
+      $edit_node = $this->parent->getEntity();
+      $editurl = $edit_node->toUrl('edit-form', $options)->toString();
       $this->list[0] = $this->createItem(0, $editurl);
     }
   }
